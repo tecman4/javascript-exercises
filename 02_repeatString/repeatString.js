8eb240c4d96d5b76f5e1299bdaf6d58b341812a2
Wayne Bonner <tecman4@gmail.com>
2023-08-16 12:29:56 -0400

    Completed exercises 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,19 @@
-const repeatString = function() {
+const repeatString = function(stringToDup,number) {
+    let stringToDuptemp=stringToDup;
+        if(number>0){
+        for(i=0;i<number-1;i++){
+            stringToDup=stringToDup+stringToDuptemp;   
+        }
+    }
+
+    if(number==0){
+
+        stringToDup= ""
+    }
+    if(number<0){
+        stringToDup="ERROR"
+        }
+return stringToDup
 
 };
 
