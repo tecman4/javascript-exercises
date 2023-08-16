d8b66ec13de7b6c83bbccb273dd8cdbf2a6456e3
Wayne Bonner <tecman4@gmail.com>
2023-08-16 14:13:31 -0400

    Completed Exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,18 @@
-const reverseString = function() {
+const reverseString = function(stringToReverse) {
+    let stringToReversetemp=stringToReverse;
+    stringToReverse="";
+for(let i=stringToReversetemp.length-1;i>=0;i--){
+    stringToReverse+=stringToReversetemp[i];
 
+}
+if(stringToReversetemp.length==0){
+
+    stringToReverse= ""
+}
+if(stringToReversetemp.length<0){
+    stringToReverse="ERROR"
+    }
+return stringToReverse
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function(array,toRemove) {
+    let arrayTemp = new Array();
 
-};
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
