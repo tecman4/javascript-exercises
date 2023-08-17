92b03ecaee519ce31ad5a8034e60357330c72833
Wayne Bonner <tecman4@gmail.com>
2023-08-17 16:22:35 -0400

    Completed Exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,7 +1,12 @@
-const removeFromArray = function(array,toRemove) {
+const removeFromArray = function(array, ...toRemove) {
     let arrayTemp = new Array();
 
-
+    array.forEach((item) => {if (!toRemove.includes(item)) {
+        arrayTemp.push(item);
+      }
+    });
+return arrayTemp
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
