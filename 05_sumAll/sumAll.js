1a8ffba6502cc7acbe9e465663e67bfddb489e41
Wayne Bonner <tecman4@gmail.com>
2023-08-30 16:28:25 -0400

    Completed Exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
+    let sum =0
 
+ //   array.forEach((item) => {if (!toRemove.includes(item)) {
+        
+     // }
+    //});
+//return arrayTemp
+if(min<0 || max<0) return "ERROR"
+if(!Number.isInteger(min) || !Number.isInteger(max)) return "ERROR"
+if(max<min){
+    let temp =min;
+    min=max;
+    max=temp;
+}
+for(let i = min;i <= max;i++){
+    sum += i;
+}
+
+return sum
 };
 
 // Do not edit below this line
