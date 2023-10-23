76a6597c4efb6a07dee5b460f7e96e1bd03e4252
Wayne <tecman4@gmail.com>
2023-10-23 16:49:52 -0400

    Started exercise #9 Palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (array) {
+    for (i=0;i<array.length;i++){
+        for (j=array.length;j>array.length;j--){
+            if(array[i]=! array[j])
+            return false;
 
+
+        }
+    }
+return true
 };
 
 // Do not edit below this line
